2ab9d82d4967ba9bb256e69fa557963bced6c7b8
nakharinh phomsouvanh <elektrodynamiks@me.com>
2024-12-05 16:57:51 +1100

    Daily commit'

---
+++ C#/MyFirstProgram/Program.cs
@@ -0,0 +1,16 @@
+﻿// C# Characters
+char c = 'a';
+char d = '1';
+char s = '*';
+int i = sizeof(char); // i = 2
+Console.WriteLine(i);
+
+Random dice = new();
+int roll = dice.Next();
+Console.WriteLine(roll);
+
+int firstValue = 500;
+int secondValue = 600;
+int largerValue = Math.Max(firstValue, secondValue);
+
+Console.WriteLine(largerValue);

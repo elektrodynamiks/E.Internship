f65377549988780c7395fe217eabc29f7af7623c
nakharinh phomsouvanh <elektrodynamiks@me.com>
2024-12-11 09:43:21 +1100

    aof

--- C#/MyFirstProgram/obj/Release/net9.0/MyFirstProgram.AssemblyInfo.cs
+++ C#/MyFirstProgram/obj/Release/net9.0/MyFirstProgram.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyFirstProgram")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+12c873251f8d4413b115f491365a23cd9d1db740")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f363bdf5c070f7008a13a962aaef2fac9787c04a")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyFirstProgram")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyFirstProgram")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

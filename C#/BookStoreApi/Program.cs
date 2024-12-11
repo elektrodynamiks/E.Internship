0c41a583c353ed454fa8b8fc320cd421b2b8a084
nakharinh phomsouvanh <elektrodynamiks@me.com>
2024-12-12 10:14:41 +1100

    webapi

---
+++ C#/BookStoreApi/Program.cs
@@ -0,0 +1,76 @@
+using System;
+using System.IO;
+using System.Reflection;
+using BookStoreApi.Models;
+using BookStoreApi.Services;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSingleton<BooksService>();
+builder.Services.AddControllers();
+builder.Services.AddOpenApiDocument(config =>
+{
+    config.DocumentName = "BookStoreApi";
+    config.Title = "BookStoreApi v1";
+    config.Version = "v1";
+});
+builder.Services.Configure<BookStoreDatabaseSettings>(
+    builder.Configuration.GetSection("BookStoreDatabase")
+);
+
+var app = builder.Build();
+if (app.Environment.IsDevelopment())
+{
+    app.UseOpenApi();
+    app.UseSwaggerUi(config =>
+    {
+        config.DocumentTitle = "BookStoreApi";
+        config.Path = "/swagger";
+        config.DocumentPath = "/swagger/{documentName}/swagger.json";
+        config.DocExpansion = "list";
+    });
+}
+
+// Configure the HTTP request pipeline.
+
+app.UseHttpsRedirection();
+
+var summaries = new[]
+{
+    "Freezing",
+    "Bracing",
+    "Chilly",
+    "Cool",
+    "Mild",
+    "Warm",
+    "Balmy",
+    "Hot",
+    "Sweltering",
+    "Scorching",
+};
+
+app.MapGet(
+    "/weatherforecast",
+    () =>
+    {
+        var forecast = Enumerable
+            .Range(1, 5)
+            .Select(index => new WeatherForecast(
+                DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+                Random.Shared.Next(-20, 55),
+                summaries[Random.Shared.Next(summaries.Length)]
+            ))
+            .ToArray();
+        return forecast;
+    }
+);
+
+app.Run();
+
+record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
+{
+    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+}
---
+++ C#/BookStoreApi/read.md
@@ -0,0 +1,24 @@
+# Create a web API with ASP.NET Core and MongoDB.
+
+https://learn.microsoft.com/en-us/aspnet/core/tutorials/first-mongo-app?view=aspnetcore-9.0&tabs=visual-studio-code
+
+Run the app
+CLI:
+dotnet run --launch-profile https
+
+# Install MongoDB.
+
+# Create the ASP.NET Core web API project.
+
+https://learn.microsoft.com/en-us/training/modules/build-web-api-aspnet-core/3-exercise-create-web-api
+
+CLI:
+dotnet new webapi -o BookStoreApi --no-openapi true
+code BookStoreApi
+
+# Add Swagger to the project.
+
+https://learn.microsoft.com/en-us/aspnet/core/tutorials/min-web-api?view=aspnetcore-8.0&tabs=visual-studio
+// dotnet add TodoApi.csproj package Swashbuckle.AspNetCore -v 6.6.2
+NET CLI:
+dotnet add BookStoreApi.csproj package NSwag.AspNetCore
